15142a912315e9704efb09740d36c64c82b08912
xs2811949745 <115071348+xs2811949745@users.noreply.github.com>
2022-11-19 23:51:47 +0800

    Add files via upload

--- C.user.h
+++ C.user.h
@@ -5,56 +5,23 @@
 #include <string.h>
 #include <windows.h>
 #include <stdio.h>
-int prime(int n)	//素数判断
-{
 
-	int flag = 1;	//素数返回值为1
-	for (int i = 2; i <=sqrt(n); i++)
-	{
-		if (n % i == 0)
-		{
-			flag = 0;
-			break;
-		}
-	}
-	if (n == 1)
-		flag = 0;	//非素数返回0
-	if (n == 2 || n == 3)
-	{
-		flag = 1;
-	}
-	return flag;
-}
-int vowel(char ch)		//	判断元音
-{
-	int flag = 0;
-	if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
-	{
-		flag = 1;
-	}
-	return flag;
-}
-int HmsToS(int h, int m, int s)
-{
-	int end = h * 3600 + m * 60 + s;
-	return end;
-	
-}
-void PrintTime(int c)
-{
-	int h=0, m=0, s;
-	s = c;
-	while (s > 60)
-	{
-		{
-			s -= 60;
-		m++; }
-		while (m > 60)
-		{
-			m -= 60;
-			h++;
-		}
-	}
-			printf("%02d:%02d:%02d", h, m, s);
 
-}
+
+int prime(int n);	//素数判断
+
+int vowel(char ch);	//	判断元音
+
+int HmsToS(int h, int m, int s);  //计算时间多少秒
+
+void PrintTime(int c); //打印时间差
+
+int Narcissus(int a);  //判断水仙花
+
+int find_same_string_num(char* atr1, char* str2);//返回相同字符串出现次数   ,只能用scanf输入
+
+void del(int a[], int n, int i);  /*删除数组a中下标为i的元素*/
+
+void PrintArr(int a[], int n); /*输出数组a的前n个元素*/
+
+int sort(int a[], int n);	/*冒泡排序（未优化）*/
