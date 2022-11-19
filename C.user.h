2f8b4467a835e4515f0dd63f8a4ff984d8b3a2ee
xs2811949745 <115071348+xs2811949745@users.noreply.github.com>
2022-11-19 23:44:50 +0800

    Add files via upload

--- C.user.h
+++ C.user.h
@@ -12,8 +12,16 @@ int prime(int n);	//素数判断
 
 int vowel(char ch);	//	判断元音
 
-int HmsToS(int h, int m, int s);
+int HmsToS(int h, int m, int s);  //计算时间多少秒
 
-void PrintTime(int c);
+void PrintTime(int c); //打印时间差
 
-int Narcissus(int a);
+int Narcissus(int a);  //判断水仙花
+
+int find_same_string_num(char* atr1, char* str2);//返回相同字符串出现次数   ,只能用scanf输入
+
+void del(int a[], int n, int i);  /*删除数组a中下标为i的元素*/
+
+void PrintArr(int a[], int n); /*输出数组a的前n个元素*/
+
+int sort(int a[], int n);	/*冒泡排序（未优化）*/
