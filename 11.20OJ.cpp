7b1fa375acb0a37b565633de3581ff0b89af6b42
xs2811949745 <115071348+xs2811949745@users.noreply.github.com>
2022-11-20 20:31:15 +0800

    Add files via upload

---
+++ 11.20OJ.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <windows.h>
+//float average(char *p);
+//int main()
+//{
+//	char arr[100];
+//	scanf("%s", arr);
+//	float  b = average(&arr[0]);
+//	int sz = strlen(arr);
+//	printf("%.1f", b / sz);
+//	return 0;
+//}
+//float average(char *p)
+//{
+//	int i = 0;
+//	int a = 0;
+//	while (*p != '\0')
+//	{
+//		if (*p == 'A')
+//		{
+//			a += 95;
+//		}
+//		if (*p == 'B')
+//		{
+//			a += 85;
+//		}
+//		if (*p == 'C')
+//		{
+//			a += 75;
+//		}if (*p == 'D')
+//		{
+//			a += 65;
+//		}if (*p == 'E')
+//		{
+//			a += 40;
+//		}
+//		p++;
+//	}
+//	return a;
+//}
