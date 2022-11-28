71fb982d5ed13e77722dbfc22a967a6eaf8ed613
xs2811949745 <115071348+xs2811949745@users.noreply.github.com>
2022-11-28 23:30:35 +0800

    Add files via upload

---
+++ C.test.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <windows.h>
+#include "C_test_user.h"
+extern students stu[10000];
+int main()
+{
+	
+	menu();
+	return 0;
+}
