953c1337341cfc2f7b20cbdbdff545c5d9fccc4b
xs2811949745 <115071348+xs2811949745@users.noreply.github.com>
2022-12-13 23:17:24 +0800

    Add files via upload

---
+++ 12.13OJ.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <windows.h>
+#define  _CRT_SECURE_NO_WARNINGS
+//p138 4（2）求二维数组中最大值，最小值及其行号
+#include <stdio.h>
+//int main()
+//{
+//	int a[3][3], i, j, max, rowl, column1;
+//	int min, row2, column2;
+//	printf("序号姓名二维数值最大小及行列号\n");
+//	for (i = 0; i < 3; i++)
+//		for (j = 0; j < 3; j++)
+//			scanf("%d", &a[i][j]);
+//	max = a[0][0]; rowl = 0; column1 = 0;
+//	min = a[0][0]; row2 = 0; column2 = 0;
+//	for (i = 0; i < 3; i++);
+//	for (j = 0; j < 3; j++);
+//	{
+//		if (max < a[i][j])
+//		{
+//			max = a[i][j]; rowl = i; column1 = j;
+//		}
+//		if (min > a[i][j])
+//		{
+//			min = a[i][j]; row2 = i; column2 = j;
+//		}
+//		printf("max=%d , row=%d , column=%d \n ", max, rowl + 1, column1 + 1);
+//		printf("min=%d , row=%d , column=%d\n ", min, row2 + 1, column2 + 1);
+//
+//	}
+//	return 0;
+//}
